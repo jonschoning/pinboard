17938a459610de2f1636c9d5dbd9861eaf319a35
Jon Schoning <jonschoning@gmail.com>
2016-04-07 17:10:37 -0500

    update version

--- src/Pinboard/Client.hs
+++ src/Pinboard/Client.hs
@@ -155,7 +155,7 @@ buildReq :: MonadIO m => String -> m Request
 buildReq url = do
   req <- liftIO $ parseUrl $ "https://api.pinboard.in/v1/" <> url
   return $ req 
-    { requestHeaders = [("User-Agent","pinboard.hs/0.9.4")]
+    { requestHeaders = [("User-Agent","pinboard.hs/0.9.5")]
     , checkStatus = \_ _ _ -> Nothing
     }
 
