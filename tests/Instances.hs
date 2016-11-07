5c0d78d503dfea3f684fe97238319334dddf71d6
Jon Schoning <jonschoning@gmail.com>
2016-11-07 11:29:14 -0600

    hfmt

--- tests/Instances.hs
+++ tests/Instances.hs
@@ -1,11 +1,11 @@
 module Instances where
 
-import           Data.Text (Text, pack)
-import           Data.Char (isSpace)
-import           Data.List (sort)
-import           Data.Time.Calendar (Day(..))
-import           Data.Time.Clock (UTCTime(..), secondsToDiffTime)
-import           Test.QuickCheck
+import Data.Text (Text, pack)
+import Data.Char (isSpace)
+import Data.List (sort)
+import Data.Time.Calendar (Day(..))
+import Data.Time.Clock (UTCTime(..), secondsToDiffTime)
+import Test.QuickCheck
 import qualified Data.HashMap.Strict as HM
 import qualified Data.Set as Set
 
@@ -20,73 +20,74 @@ instance Arbitrary Day where
   shrink = (ModifiedJulianDay <$>) . shrink . toModifiedJulianDay
 
 instance Arbitrary UTCTime where
-  arbitrary = UTCTime <$> arbitrary <*> (secondsToDiffTime <$> choose (0, 86401))
+  arbitrary =
+    UTCTime <$> arbitrary <*> (secondsToDiffTime <$> choose (0, 86401))
 
 instance Arbitrary Note where
-  arbitrary = Note <$> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
+  arbitrary =
+    Note <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*>
+    arbitrary <*>
+    arbitrary
 
 instance Arbitrary NoteList where
   arbitrary = NoteList <$> arbitrary <*> resize 15 arbitrary
 
 instance Arbitrary NoteListItem where
-  arbitrary = NoteListItem <$> arbitrary
-                           <*> arbitrary
-                           <*> arbitrary
-                           <*> arbitrary
-                           <*> arbitrary
-                           <*> arbitrary
+  arbitrary =
+    NoteListItem <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*>
+    arbitrary
 
 instance Arbitrary Posts where
   arbitrary = Posts <$> arbitrary <*> arbitrary <*> resize 15 arbitrary
 
 instance Arbitrary Post where
-  arbitrary = Post <$> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitrary
-                   <*> arbitraryTags
+  arbitrary =
+    Post <$> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*> arbitrary <*>
+    arbitrary <*>
+    arbitrary <*>
+    arbitrary <*>
+    arbitraryTags
 
 instance Arbitrary JsonTagMap where
-  arbitrary = ToJsonTagMap <$> (HM.fromList <$> listOf ((,) <$> arbitraryTag <*> arbitrary))
+  arbitrary =
+    ToJsonTagMap <$>
+    (HM.fromList <$> listOf ((,) <$> arbitraryTag <*> arbitrary))
 
 arbitraryTags :: Gen [Tag]
 arbitraryTags = listOf arbitraryTag
 
 arbitraryTag :: Gen Tag
-arbitraryTag = pack <$> listOf1 (arbitrary `suchThat` (\c -> (not . isSpace) c && (',' /= c)))
+arbitraryTag =
+  pack <$>
+  listOf1 (arbitrary `suchThat` (\c -> (not . isSpace) c && (',' /= c)))
 
 -- | Checks if a given list has no duplicates in _O(n log n)_.
-hasNoDups :: (Ord a) => [a] -> Bool
+hasNoDups
+  :: (Ord a)
+  => [a] -> Bool
 hasNoDups = go Set.empty
   where
     go _ [] = True
     go s (x:xs)
-      | s' <- Set.insert x s,
-        Set.size s' > Set.size s
-      = go s' xs
+      | s' <- Set.insert x s
+      , Set.size s' > Set.size s = go s' xs
       | otherwise = False
 
 instance Arbitrary PostDates where
-  arbitrary = PostDates <$> arbitrary <*> arbitrary <*> (arbitrary `suchThat` isValidDateCount)
+  arbitrary =
+    PostDates <$> arbitrary <*> arbitrary <*>
+    (arbitrary `suchThat` isValidDateCount)
     where
       isValidDateCount xs = hasNoDups (fst <$> xs) && all (> 0) (snd <$> xs)
 
-instance ApproxEq Day where (=~) = (==)
+instance ApproxEq Day where
+  (=~) = (==)
+
 instance ApproxEq PostDates where
   (=~) a b =
-    postDatesUser a =~ postDatesUser b
-    && postDatesTag a =~ postDatesTag b
-    && sort (postDatesCount a) =~ sort (postDatesCount b)
+    postDatesUser a =~ postDatesUser b &&
+    postDatesTag a =~ postDatesTag b &&
+    sort (postDatesCount a) =~ sort (postDatesCount b)
 
 instance Arbitrary Suggested where
   arbitrary = arbitrary >>= \a -> elements [Popular a, Recommended a]
--- tests/Test.hs
+++ tests/Test.hs
@@ -4,50 +4,53 @@
 
 module Main where
 
-import           Data.Time.Clock (UTCTime(..))
-import           Data.Typeable (Proxy(..))
-import           Test.Hspec
-import           Test.Hspec.QuickCheck (prop)
+import Data.Time.Clock (UTCTime(..))
+import Data.Typeable (Proxy(..))
+import Test.Hspec
+import Test.Hspec.QuickCheck (prop)
 
 import PropJSON
-import Instances()
+import Instances ()
 
-import           Pinboard
+import Pinboard
 
 main :: IO ()
-main = hspec $ do
-
-  prop "UTCTime" $ \(x :: UTCTime) -> (readNoteTime . showNoteTime) x == (return x :: Maybe UTCTime)
-
-  describe "JSON instances" $ do
-    propJSONEq (Proxy :: Proxy UTCTime)
-    propJSONEq (Proxy :: Proxy Post)
-    propJSONEq (Proxy :: Proxy Posts)
-    propJSONEq (Proxy :: Proxy Note)
-    propJSONEq (Proxy :: Proxy NoteList)
-    propJSONEq (Proxy :: Proxy NoteListItem)
-    propJSONEq (Proxy :: Proxy JsonTagMap)
-    propJSONEq (Proxy :: Proxy Suggested)
-    propJSONApproxEq (Proxy :: Proxy PostDates)
-
-    describe "decodeJSONResponse: handle parse failures" $ do
-        it "malformed object parses as ParseFailure" $
-            let noteJson = "FAIL"
-            in case decodeJSONResponse noteJson of
-            Left PinboardError{..} -> errorType == ParseFailure
-            Right Note{..} -> False
-        it "malformed field parses as ParseFailure" $
-            let noteJson = "{\"length\":0,\"hash\":\"\",\"text_FAIL\":\"\",\"updated_at\":\"1864-05-09 13:50:53\",\"created_at\":\"1864-05-09 18:21:35\",\"id\":\"\",\"title\":\"\"}"
-            in case decodeJSONResponse noteJson of
-            Left PinboardError{..} -> errorType == ParseFailure
-            Right Note{..} -> False
-        it "malformed value parses as ParseFailure" $
-            let noteJson = "{\"length\":FAIL,\"hash\":\"\",\"text\":\"\",\"updated_at\":\"1864-05-09 13:50:53\",\"created_at\":\"1864-05-09 18:21:35\",\"id\":\"\",\"title\":\"\"}"
-            in case decodeJSONResponse noteJson of
-            Left PinboardError{..} -> errorType == ParseFailure
-            Right Note{..} -> False
-        it "malformed time parses as ParseFailure" $
-            let noteJson = "{\"length\":0,\"hash\":\"\",\"text\":\"\",\"updated_at\":\"FAIL-05-09 13:50:53\",\"created_at\":\"1864-05-09 18:21:35\",\"id\":\"\",\"title\":\"\"}"
-            in case decodeJSONResponse noteJson of
-            Left PinboardError{..} -> errorType == ParseFailure
-            Right Note{..} -> False
+main =
+  hspec $
+  do prop "UTCTime" $
+       \(x :: UTCTime) ->
+          (readNoteTime . showNoteTime) x == (return x :: Maybe UTCTime)
+     describe "JSON instances" $
+       do propJSONEq (Proxy :: Proxy UTCTime)
+          propJSONEq (Proxy :: Proxy Post)
+          propJSONEq (Proxy :: Proxy Posts)
+          propJSONEq (Proxy :: Proxy Note)
+          propJSONEq (Proxy :: Proxy NoteList)
+          propJSONEq (Proxy :: Proxy NoteListItem)
+          propJSONEq (Proxy :: Proxy JsonTagMap)
+          propJSONEq (Proxy :: Proxy Suggested)
+          propJSONApproxEq (Proxy :: Proxy PostDates)
+          describe "decodeJSONResponse: handle parse failures" $
+            do it "malformed object parses as ParseFailure" $
+                 let noteJson = "FAIL"
+                 in case decodeJSONResponse noteJson of
+                      Left PinboardError {..} -> errorType == ParseFailure
+                      Right Note {..} -> False
+               it "malformed field parses as ParseFailure" $
+                 let noteJson =
+                       "{\"length\":0,\"hash\":\"\",\"text_FAIL\":\"\",\"updated_at\":\"1864-05-09 13:50:53\",\"created_at\":\"1864-05-09 18:21:35\",\"id\":\"\",\"title\":\"\"}"
+                 in case decodeJSONResponse noteJson of
+                      Left PinboardError {..} -> errorType == ParseFailure
+                      Right Note {..} -> False
+               it "malformed value parses as ParseFailure" $
+                 let noteJson =
+                       "{\"length\":FAIL,\"hash\":\"\",\"text\":\"\",\"updated_at\":\"1864-05-09 13:50:53\",\"created_at\":\"1864-05-09 18:21:35\",\"id\":\"\",\"title\":\"\"}"
+                 in case decodeJSONResponse noteJson of
+                      Left PinboardError {..} -> errorType == ParseFailure
+                      Right Note {..} -> False
+               it "malformed time parses as ParseFailure" $
+                 let noteJson =
+                       "{\"length\":0,\"hash\":\"\",\"text\":\"\",\"updated_at\":\"FAIL-05-09 13:50:53\",\"created_at\":\"1864-05-09 18:21:35\",\"id\":\"\",\"title\":\"\"}"
+                 in case decodeJSONResponse noteJson of
+                      Left PinboardError {..} -> errorType == ParseFailure
+                      Right Note {..} -> False
